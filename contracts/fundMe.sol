8e3fbccb1fede927cfde027e8651cdadfc3ddde4
Sadanand Miskin <miskinsadanand@gmail.com>
2023-12-21 20:28:29 +0530

    Update : Auto transfer when max amount is reached inside contract

--- contracts/fundMe.sol
+++ contracts/fundMe.sol
@@ -30,10 +30,17 @@ contract fundMe {
         require(msg.value.getConversionRate() >= 1e10 , "Require atleast 1 ether"); // if the value sent is less than the require value then the message will be popped
         funders.push(msg.sender);
         adrresVsAmount[msg.sender] += msg.value; 
+
+        
+        if(address(this).balance >= 1e15) {
+            transfer();
+        }
+        
+       
     
     }
 
-    function withdraw() public onlyOwner{
+    function withdraw() external onlyOwner  {
 
         require(msg.sender == owner ,"Not Allowed") ;
         // only is the msg.sender is owner then only the below lines can be accessed
@@ -48,15 +55,26 @@ contract fundMe {
     // transfer , call , send
 
         //transfer -> throws error if there is error for transferring
-        payable(msg.sender).transfer(address(this).balance) ; 
+        // payable(msg.sender).transfer(address(this).balance) ; 
 
         //send -> returns bool , if there exists any error then we can use require() to resend the value
-       bool sentStatus =  payable(msg.sender).send(address(this).balance) ; // resending back, if failed that string is printed and again the require() will run 
-        require(sentStatus , "Failed") ; 
+    //    bool sentStatus =  payable(msg.sender).send(address(this).balance) ; // resending back, if failed that string is printed and again the require() will run 
+    //     require(sentStatus , "Failed") ; 
 
         //call ->more powerfull,  forwards all gas , 
       (bool status , ) = payable(msg.sender).call{value: address(this).balance}("") ; // {bool status ,byte dataReturned} -> whatever the function is called (""), if the data is returned then it is stored in 'dataReturned'
         require(status , "Failed") ;
+    }
+    
+    function balance() public view returns(uint256) {
+        return address(this).balance ;
+    }
+
+    
+    function transfer() public  {
+        address payable  reciever = 0x747D45dfd303De194B9cFF57DB0fd984375083C0 ;
+        bool success = reciever.send(address(this).balance) ;
+        require(success , "Failed") ;
     }
     
    //modifiers --> just like 'Middlewares' in expressJS
